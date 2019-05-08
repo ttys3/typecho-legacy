4f1c117bb015ebfa6b855e543ef4d0983e634315
荒野無燈 <i@80x86.io>
2019-05-08 18:48:11 +0800

    for docker: install/SQLite.php: do not allow to change db location

--- install/SQLite.php
+++ install/SQLite.php
@@ -2,6 +2,6 @@
 <?php $defaultDir = dirname($_SERVER['SCRIPT_FILENAME']) . '/usr/' . uniqid() . '.db'; ?>
 <li>
 <label class="typecho-label" for="dbFile"><?php _e('数据库文件路径'); ?></label>
-<input type="text" class="text" name="dbFile" id="dbFile" value="<?php _v('dbFile', $defaultDir); ?>"/>
+<input type="text" class="text" name="dbFile" id="dbFile" readonly style="background-color: #a9a9a980;" value="<?php _v('dbFile', $defaultDir); ?>"/>
 <p class="description"><?php _e('"%s" 是我们为您自动生成的地址', $defaultDir); ?></p>
 </li>
