59fc0d7c06c12a9da686798d69abf1491a80ac4c
荒野無燈 <i@80x86.io>
2019-05-09 22:50:12 +0800

    admin/copyright.php: add qq group link

--- admin/copyright.php
+++ admin/copyright.php
@@ -5,6 +5,12 @@
         <p><?php _e('由 <a href="http://typecho.org">%s</a> 强力驱动, 版本 %s (%s)', $options->software, $prefixVersion, $suffixVersion); ?></p>
     </div>
     <nav class="resource">
+        <a target="_blank" href="//shang.qq.com/wpa/qunwpa?idkey=b6421e379e86d5d22db97de6f5e42f82e95d37c30c0421fa4d90825d1e3773c1">
+            NanoDM固件交流群
+        </a>
+        <a target="_blank" href="http://nanodm.net">
+            NanoDM官网
+        </a>
         <a href="http://docs.typecho.org"><?php _e('帮助文档'); ?></a> &bull;
         <a href="http://forum.typecho.org"><?php _e('支持论坛'); ?></a> &bull;
         <a href="https://github.com/typecho/typecho/issues"><?php _e('报告错误'); ?></a> &bull;
